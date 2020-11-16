f49e717459a08438e50c85eee10d70eb5700d870
suparli1208 <suparli1208@gmail.com>
2020-11-16 09:21:49 +0700

    Menambahkan Fitur Remember Me

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $autoload['packages'] = array();
 $autoload['libraries'] = array('upload', 'database', 'email', 'session', 'form_validation');
 $autoload['drivers'] = array();
-$autoload['helper'] = array('url', 'file', 'security', 'aws');
+$autoload['helper'] = array('url', 'file', 'security', 'aws', 'cookie');
 $autoload['config'] = array();
 $autoload['language'] = array();
 $autoload['model'] = array();
--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -13,10 +13,37 @@ class Auth extends CI_Controller
 
 	public function index()
 	{
+		
 		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
 		$this->form_validation->set_rules('password', 'Password', 'required|trim');
+		$cookie = get_cookie('dataonline');	
 
+		if ($this->session->userdata('logged')) {
+            if ($this->session->userdata('rule_id') == 1) {
+				redirect('admin');
+			} else if ($this->session->userdata('rule_id') == 2) {
+				$datadevice = $this->admin->getInformation();
+				redirect('user');
+			} 
+		} 
+		else if($cookie <> ''){
+			$key = $this->db->get_where('user', ['key' => $cookie])->row_array();
+			if ($key) {
+				$session = [
+					'id_user' => $key['id_user'],
+					'email' => $key['email'],
+					'rule_id' => $key['rule_id'],
+					'logged' => TRUE
+
+				];
+				$this->session->set_userdata($session);
+				$this->_login();
+
+			}
+
+		}
 		if ($this->form_validation->run() == false) {
+			
 
 			$data['title'] = 'Login - Aws';
 
@@ -31,6 +58,7 @@ class Auth extends CI_Controller
 	{
 		$email = $this->input->post('email');
 		$password = $this->input->post('password');
+		$remember = $this->input->post('remember');
 
 		$user = $this->db->get_where('user', ['email' => $email])->row_array();
 		//jika usernya ada
@@ -45,27 +73,35 @@ class Auth extends CI_Controller
 			if ($user['is_active'] == 1) {
 				//cek passwordnya
 				if (password_verify($password, $user['password'])) {
-
-
-
-
 					$data = [
 						'id_user' => $user['id_user'],
 						'email' => $user['email'],
-						'rule_id' => $user['rule_id']
+						'rule_id' => $user['rule_id'],
+						'logged' => TRUE
 
 					];
-					$this->session->set_userdata($data);
-
-
-					if ($user['rule_id'] == 1) {
-						redirect('admin');
-					} else {
-
-
-						$datadevice = $this->admin->getInformation();
-						redirect('user');
+					
+					if ($remember) {
+						$key = random_string('alnum', 64);
+						var_dump($key);
+						set_cookie('dataonline', $key, 3600*24*360); // set expired 30 hari kedepan
+						// simpan key di database
+						$update_key = array(
+							'key' => $key
+						);
+						$this->db->where('id_user', $user['id_user']);
+						$this->db->update('user', $update_key);
 					}
+					$this->session->set_userdata($data);
+					
+						if ($user['rule_id'] == 1) {
+							redirect('admin');
+						} else {
+
+
+							$datadevice = $this->admin->getInformation();
+							redirect('user');
+						}
 				} else {
 					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Salah</div>');
 					redirect('auth');
@@ -85,6 +121,8 @@ class Auth extends CI_Controller
 	{
 		$this->session->unset_userdata('email');
 		$this->session->unset_userdata('rule_id');
+		$this->session->unset_userdata('logged');
+		delete_cookie('dataonline');
 		$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> You has been logout </div>');
 		redirect('auth');
 	}
